397c735a20631baade0b73533b7f6db894c84d6b
jamesgov <blindmice1o3@gmail.com>
2022-09-04 17:45:27 -0700

    tinker: Learn Architecture Components from codelab Android Room with a View
    https://developer.android.com/codelabs/android-room-with-a-view

---
+++ app/src/main/java/com/jackingaming/autopiloton/database/entities/Word.java
@@ -0,0 +1,33 @@
+package com.jackingaming.autopiloton.database.entities;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "word_table")
+public class Word {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @NonNull
+    @ColumnInfo(name = "word")
+    private String word;
+
+    public Word(@NonNull String word) {
+        this.word = word;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getWord() {
+        return word;
+    }
+}
